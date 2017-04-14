8b1575817762a3b8a582b5b2c3ffe18d65e198cb
Austin Chen <chenas@smu.edu>
2017-04-14 18:29:53 -0500

    added vagrant file, sql files

---
+++ bobbytables.sql
@@ -0,0 +1,8 @@
+DROP TABLE `Users`;
+DROP TABLE `Logins`;
+DROP TABLE `ViewEngagement`;
+DROP TABLE `TweetData`;
+DROP TABLE `HourlyData`;
+DROP TABLE `WeeklyData`;
+DROP TABLE `TopWords`;
+DROP TABLE `TopHashtags`;
