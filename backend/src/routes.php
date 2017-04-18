4e447a0aa21985fd10a3223c891570d14429e948
ovallner <ovallner@gmail.com>
2017-04-18 03:21:34 +0000

    more endpoints work

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -12,7 +12,32 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 });
 
 $app->post('/user/', function($request, $response) {
-  $data = $request->getParsedBody();
+    $data = $request->getParsedBody();
+    // Check if the email is unique 
+    $emailqry = "SELECT * FROM Users WHERE email =:email";
+    $emailqry->bindParam("email", $data['email']);
+    $emailqry->execute();
+    $emailrslt = $emailqry->fetchAll();
+    if ($emailrslt.count() > 0)
+    {
+        $valid = json_encode(array('success' => False));
+        return $this->response->$valid;
+        echo "You messed up";
+    }
+    
+    // Check if the twitter handle is unique 
+    $handleqry = "SELECT * FROM Users WHERE twitter_handle =:twitter_handle";
+    $handleqry->bindParam("twitter_handle", $data['twitter_handle']);
+    $handleqry->execute();
+    $handlerslt = $handleqry->fetchAll();
+    if ($handlerslt.count() > 0)
+    {
+        $valid = json_encode(array('success' => False));
+        return $this->response->$valid;
+        echo "You messed up";
+    }
+
+
     $sql = "INSERT INTO Users (email, first_name, password, twitter_handle, api_key, api_secret, created_at) VALUES (:email, :first_name, :password, :handle, :key, :secret, NOW())";
     $sth = $this->db->prepare($sql);
     $sth->bindParam("email", $data['email']);
@@ -26,11 +51,14 @@ $app->post('/user/', function($request, $response) {
     return $this->response->withJson($data);
 });
 
-$app->get('/user/[{twitter_handle}]', function($request, $response, $args) {
+$app->get('/user/info/[{twitter_handle}]', function($request, $response, $args) {
     $sth = $this->db->prepare("SELECT * FROM Users WHERE twitter_handle=:twitter_handle");
     $sth->bindParam("twitter_handle", $args['twitter_handle']);
     $sth->execute();
     $todos = $sth->fetchObject();
     echo "\n";
+    return $this->response->withJson($todos);
 });
+
+
 
