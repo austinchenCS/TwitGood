1f099153bbff2936164f83591d0aaf9593c41207
Eden <Eden@eden-mbp.local>
2017-04-19 18:46:57 -0500

    First commit

--- twitgoodDB.sql
+++ twitgoodDB.sql
@@ -14,6 +14,14 @@ ALTER TABLE `Users` DROP PRIMARY KEY;
 ALTER TABLE `Users` ADD PRIMARY KEY(`user_id`);
 ALTER TABLE `Users` MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 
+INSERT INTO `Users` (`email`, `first_name`, `password`, `twitter_handle`, `api_key`, `api_secret`, `created_at`)
+VALUES ('one@gmail.gov', 'Jerry Knowles', 'password', 'jknowles', '82329847298432', '23974142397 12834792374', NOW()),
+('two@gmail.gov', 'Jenny', 'password', 'j_malo', '82329847298432', '23974142397 12834792374', NOW()),
+('three@gmail.gov', 'gargomel', 'password', 'i_got_truth', '82329847298432', '23974142397 12834792374', NOW()),
+('four@gmail.gov', 'It me', 'password', 'fentibego', '82329847298432', '23974142397 12834792374', NOW()),
+('five@gmail.gov', 'Garnet', 'password', 'garnet_love_time', '82329847298432', '23974142397 12834792374', NOW()),
+('six@gmail.gov', 'XMentosFan2012X', 'password', 'xmentosfan2012x', '82329847298432', '23974142397 12834792374', NOW());
+
 CREATE TABLE IF NOT EXISTS `Logins` (
   `user_id` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -36,13 +44,21 @@ ALTER TABLE `ViewEngagement` ADD FOREIGN KEY(`user_id`) REFERENCES `Users`(`user
 CREATE TABLE IF NOT EXISTS `TweetData` (
   `user_id` int(11) NOT NULL,
   `top_tweet` varchar(150) NOT NULL,
-  `account_age` varchar(15) NOT NULL,
+  `account_age` varchar(35) NOT NULL,
   `created` timestamp NOT NULL,
   `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `TweetData` ADD PRIMARY KEY(`user_id`);
-ALTER TABLE `TweetData` ADD FOREIGN KEY(`user_id`) REFERENCES `Users`(`user_id`); 
+ALTER TABLE `TweetData` ADD FOREIGN KEY(`user_id`) REFERENCES `Users`(`user_id`);
+
+INSERT INTO `TweetData` (`user_id`, `top_tweet`, `account_age`, `created`)
+VALUES ('1', 'This is my top tweet.', 'Sat Dec 14 04:35:55 +0000 2013', NOW()),
+('4', 'Hey, this is my top tweet.', 'Sat Dec 14 04:35:55 +0000 2013', NOW()),
+('2', 'I love to tweet.', 'Sat Dec 14 04:35:55 +0000 2013', NOW()),
+('3', 'Great website.', 'Sat Dec 14 04:35:55 +0000 2013', NOW()),
+('5', 'Big fan of the blue bird website that I\'m on!', 'Sat Dec 14 04:35:55 +0000 2013', NOW()),
+('6', 'This is also *my* top tweet.', 'Sat Dec 14 04:35:55 +0000 2013', NOW()); 
 
 CREATE TABLE IF NOT EXISTS `HourlyData` (
   `user_id` int(11) NOT NULL,
@@ -55,6 +71,19 @@ CREATE TABLE IF NOT EXISTS `HourlyData` (
 
 ALTER TABLE `HourlyData` ADD PRIMARY KEY(`user_id`, `hour`);
 ALTER TABLE `HourlyData` ADD FOREIGN KEY(`user_id`) REFERENCES `Users`(`user_id`); 
+
+INSERT INTO `HourlyData` (`user_id`, `hour`, `activity`, `success`, `created`)
+VALUES ('3', `00`, `0`, `3`, NOW()), ('3', `01`, `0`, `2`, NOW()),
+('3', `02`, `0`, `0`, NOW()), ('3', `03`, `0`, `1`, NOW()),
+('3', `04`, `0`, `18`, NOW()), ('3', `05`, `0`, `3`, NOW()),
+('3', `06`, `0`, `2`, NOW()), ('3', `07`, `1`, `3`, NOW()),
+('3', `08`, `3`, `5`, NOW()), ('3', `09`, `2`, `6`, NOW()),
+('3', `10`, `0`, `9`, NOW()), ('3', `11`, `5`, `20`, NOW()),
+('3', `12`, `6`, `21`, NOW()), ('3', `13`, `4`, `31`, NOW()),
+('3', `14`, `18`, `68`, NOW()), ('3', `15`, `6`, `196`, NOW()),
+('3', `16`, `3`, `82`, NOW()), ('3', `17`, `1`, `20`, NOW()),
+('3', `18`, `12`, `5`, NOW()), ('3', `19`, `13`, `8`, NOW()),
+('3', `20`, `33`, `25`, NOW()), ('3', `21`, `4`, `41`, NOW())
 
 CREATE TABLE IF NOT EXISTS `WeeklyData` (
   `user_id` int(11) NOT NULL,
