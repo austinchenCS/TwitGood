3ec70ed790e6b3ed9a4a0a2eada0d0f805ad260e
ovallner <ovallner@gmail.com>
2017-04-20 19:37:56 +0000

    Installed tweepy and successfully tested the api

--- twitterscripts/test.py
+++ twitterscripts/test.py
@@ -3,6 +3,16 @@
 import tweepy
 
 def main():
+    #authorizes
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    query = "nba"
+    tweets = tweepy.Cursor(api.search, q = query).items()
+    for tweet in tweets:
+        print tweet.text
+
     print("hello world")
 
 
