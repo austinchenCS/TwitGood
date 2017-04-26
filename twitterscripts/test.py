ff4dc7930011506416ba7eade97b882e0c174277
Austin <chenas@smu.edu>
2017-04-26 09:53:13 +0000

    added most used hashtag functionality

--- twitterscripts/test.py
+++ twitterscripts/test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
+import re
 import tweepy
 import KEYS
 import sys
 import json
 import time
+import datetime
 from operator import itemgetter
 
 consumer_key = KEYS.get_consumer_key()
@@ -25,6 +27,9 @@ def main():
     # get list of all tweets of a user
     tweets = list(tweepy.Cursor(api.user_timeline, screen_name = user_handle).items())
 
+    # create user data thing
+    user_data = api.get_user(screen_name = user_handle)
+
     # list of tuples: (tweet, day, hour)
     time_data = []
 
@@ -42,9 +47,6 @@ def main():
     most_frequent_hashtags = []
 
     # temp variables
-    highest_favorited = 0
-    highest_retweeted = 0
-    highest_successful = 0
     all_words = []
     all_hashtags = []
 
@@ -60,25 +62,28 @@ def main():
         tweet_count += 1
         time_data.append((tweet._json, tweet._json['created_at'][:3].lower(), tweet._json['created_at'][11:13]))
 
-        curr_url = "https://twitter.com/BigSwede66/status/" + tweet._json['id_str']
+        # build URL of current tweet
+        curr_url = "https://twitter.com/twitgood/status/" + tweet._json['id_str']
 
-        #print(curr_url)
+        # get top tweets
         curr_fav = int(tweet._json['favorite_count'])
         curr_rt = int(tweet._json['retweet_count'])
         curr_success = curr_fav + curr_rt
-
-        if curr_fav > highest_favorited:
-            highest_favorited = curr_fav
-
-        if curr_rt > highest_retweeted:
-            highest_retweeted = curr_rt
-
-        if curr_success > highest_successful:
-            highest_successful = curr_success
-
         top_favorited_tweet.append((curr_url, curr_fav))
         top_retweeted_tweet.append((curr_url, curr_rt))
         top_successful_tweet.append((curr_url, curr_success))
+
+        # get hashtags
+        curr_hashtags = tweet._json['entities']['hashtags']
+        for entry in curr_hashtags:
+            if re.search('\W+', entry['text']) is None:
+                all_hashtags.append(entry['text'])
+
+
+
+    print(all_hashtags)
+        # create fat list of all words
+
 
         #print("Favs: " + str(curr_fav) + " RT: " + str(curr_rt) + " Success: " + str(curr_success))
 
@@ -91,12 +96,39 @@ def main():
     print(top_retweeted_tweet[:3])
     print(top_successful_tweet[:3])
 
-
-
-
     # Calculate account age (IN DAYS)
+    date_created = str(user_data.created_at)
+    print(date_created)
+    year_created = int(date_created[0:4])
+    month_created = int(date_created[5:7])
+    day_created = int(date_created[8:10])
+    print(year_created)
+    print(month_created)
+    print(day_created)
+
+    date_created = datetime.date(year_created, month_created, day_created)
+    today = datetime.datetime.now().date()
+
+    print(date_created)
+    print(today)
+
+    account_age = today - date_created
+    account_age = int(account_age.days)
+
+    print(account_age)
 
     # Get most frequent words
+    word_counter = {}
+    for word in all_hashtags:
+        if word in word_counter:
+            word_counter[word] += 1
+        else:
+            word_counter[word] = 1
+
+    popular_words = sorted(word_counter, key = word_counter.get, reverse = True)
+    most_frequent_hashtags = popular_words[:3]
+
+    print(most_frequent_hashtags)
 
     # Get most frequent hashtags
 
